f91516f41e043c52337c48389470b37c8566f2fd
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2024-01-24 20:21:41 -0600

    added card to appcards

--- flat-renting-app/client/src/components/apartmentDetail.js
+++ flat-renting-app/client/src/components/apartmentDetail.js
@@ -4,25 +4,30 @@ import axios from 'axios';
 
 const ApartmentDetail = () => {
   const { apartmentId } = useParams();
-  const [apartment, setApartment] = useState([])
+  const [apartment, setApartment] = useState({ images: [] }); // Initialize images as an empty array
 
   useEffect(() => {
     const fetchApartment = async () => {
-        try {
-            const response = await axios.get(`http://localhost:3001/apartments/${apartmentId}`);
-            console.log(response.data)
-            setApartment(response.data[0]);
-        } catch (error) {
-            console.error('Error fetching apartments:', error);
-        }
+      try {
+        const response = await axios.get(`http://localhost:3001/apartments/${apartmentId}`);
+        console.log(response.data);
+        setApartment(response.data[0]);
+      } catch (error) {
+        console.error('Error fetching apartments:', error);
+      }
     };
 
     fetchApartment();
-}, [apartmentId]);
+  }, [apartmentId]);
+
+  console.log(apartment.images);
 
   return (
     <div className="apartment">
       <h1>{apartment.apartment_name}</h1>
+      {apartment.images.map((image, index) => (
+        <div className='m-top hotel' key={index} style={{ backgroundImage: `url(${image})`, width: '100%', height: '300px' }}></div>
+      ))}
     </div>
   );
 };
--- flat-renting-app/client/src/components/flatCard.js
+++ flat-renting-app/client/src/components/flatCard.js
@@ -1,18 +1,16 @@
-// FlatCard.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../App.css"
+
 
 const FlatCard = ({ flat }) => {
   return (
-    <div>
 
     <div className="hotel" style={{ backgroundImage: `url(${flat.image})`}}>
       <Link to={`/flats/${flat._id}`}>
         <h2>{flat.flat_name}</h2>
       </Link>
     </div>
-    </div>
+
   );
 };
 
